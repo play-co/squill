dfeb0ad0c3bb37d7706576c1bb61af09d3f99ce4
Martin Hunt <mghunt@gmail.com>
2011-05-08 06:43:32 -0700

    fix some bugs and build out i18n support a little

--- formUtil.js
+++ formUtil.js
@@ -3,7 +3,7 @@ jsio('from util.browser import $');
 exports.onEnter = function(el, cb) {
 	if (arguments.length > 2) { cb = bind.apply(this, Array.prototype.slice.call(arguments, 1)); }
 	
-	$.onEvent(el, 'keypress', function(evt) {
+	$.onEvent(el.getElement && el.getElement() || el, 'keypress', function(evt) {
 		if (evt.keyCode == 13) {
 			cb();
 		}
@@ -11,13 +11,13 @@ exports.onEnter = function(el, cb) {
 }
 
 exports.focus = function(el) {
-	$.id(el).focus();
+	$.id(el.getElement && el.getElement() || el).focus();
 }
 
 exports.onPress = function(el, cb) {
 	if (arguments.length > 2) { cb = bind.apply(this, Array.prototype.slice.call(arguments, 1)); }
 	
-	$.onEvent(el, 'click', function(evt) {
+	$.onEvent(el.getElement && el.getElement() || el, 'click', function(evt) {
 		$.stopEvent(evt);
 		cb();
 	});
