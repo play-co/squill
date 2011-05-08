532bc5a2c1432eb66c06ec550f5128303fff8809
Martin Hunt <mghunt@gmail.com>
2011-05-08 06:43:08 -0700

    update i18n

--- i18n.js
+++ i18n.js
@@ -2,5 +2,11 @@ var gLang = null;
 
 exports.setLang = function(lang) { gLang = lang; }
 exports.get = function(key) {
-	return gLang.get(key);
+	return gLang && gLang.get(key);
 }
+
+exports.Language = Class(function() {
+	this.init = function(dict) { this._dict = dict; }
+	this.add = function(key, value) { this._dict[key] = value; }
+	this.get = function(key) { return this._dict[key]; }
+});
