225f0addba12a8edbd7cabef0bd4708ac1b18b61
Martin Hunt <mghunt@gmail.com>
2011-06-30 12:30:23 -0700

    if in devMode, listen for a CSS reload postMessage event to refresh all CSS loaded using squill.cssLoad

--- cssLoad.js
+++ cssLoad.js
@@ -3,14 +3,38 @@
 import util.ajax;
 from util.browser import $;
 
+logger.setLevel(0);
+
 exports.get = function(opts, cb) {
 	if (typeof opts == 'string') {
 		opts = {url: opts};
 	}
 	
+	logger.debug(opts.url);
 	util.ajax.get(opts.url, function(err, content) {
 		if (err) { logger.error('could not fetch css at', url); return; }
-		$({tag: 'style', text: content, parent: opts.el || document.getElementsByTagName('head')[0]});
+		var el = $({tag: 'style', text: content, parent: opts.el || document.getElementsByTagName('head')[0]});
+		
+		if (window.DEV_MODE) {
+			exports._styleTags.push({
+				el: el,
+				src: opts.url
+			});
+		}
+		
 		setTimeout(cb, 0);
 	});
+}
+
+if (window.DEV_MODE) {
+	exports._styleTags = [];
+	window.addEventListener('message', function(evt) {
+		if (evt.data == 'squill.cssLoad.reload()') {
+			for (var i = 0, s; s = exports._styleTags[i]; ++i) {
+				util.ajax.get(s.src, bind(this, function(s, err, content) {
+					if (!err) { $.setText(s.el, content); }
+				}, s));
+			}
+		}
+	}, true);
 }
