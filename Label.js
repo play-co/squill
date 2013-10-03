0f94fe94f6e4d264096e0bc31d868ea218d68218
Martin Hunt <martin@gameclosure.com>
2013-10-03 04:36:12 -0700

    use a span so a label can have child widgets

--- Label.js
+++ Label.js
@@ -1,19 +1,20 @@
-"use import";
-
 from util.browser import $;
 import squill.Widget;
 
 exports = Class(squill.Widget, function() {
 	this._css = 'label';
-	
+	this._def = {
+		children: [{
+			id: '_labelSpan',
+			tag: 'span'
+		}]
+	}
+
 	this.buildWidget = function() {
-		var el = this._el,
-			label = this.getI18n('label') || '';
-		
-		$.setText(el, label);
+		this.setLabel(this.getI18n('label'));
 	}
 	
 	this.setLabel =
-	this.setText = function(text) { $.setText(this._el, text); }
-	this.setHTML = function(html) { this._el.innerHTML = html; }
+	this.setText = function(text) { $.setText(this._labelSpan, text); }
+	this.setHTML = function(html) { this._labelSpan.innerHTML = html; }
 });
