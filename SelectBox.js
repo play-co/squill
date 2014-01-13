9c41b0549868480fa7dfba9e77a7603fccaf153f
Martin Hunt <martin@gameclosure.com>
2014-01-13 15:59:20 -0800

    wrap selects in labels for easier styling

--- SelectBox.js
+++ SelectBox.js
@@ -4,8 +4,16 @@ import squill.models.DataSource as DataSource;
 from util.browser import $;
 
 var SelectBox = exports = Class(Widget, function(supr) {
+
+	this._css = 'select';
+
 	this._def = {
-		tag: 'select'
+		tag: 'label',
+		style: {display: 'inline-block'},
+		children: [{
+			id: '_select',
+			tag: 'select'
+		}]
 	};
 
 	this.init = function(opts) {
@@ -17,7 +25,7 @@ var SelectBox = exports = Class(Widget, function(supr) {
 		if (opts.dataSource) { this.setDataSource(opts.dataSource); }
 		supr(this, 'init', arguments);
 	}
-	
+
 	this.buildWidget = function() {
 		if (this._opts.name) {
 			this.setName(this._opts.name);
@@ -26,7 +34,7 @@ var SelectBox = exports = Class(Widget, function(supr) {
 		if (!this._dataSource) {
 			this.setDataSource(new DataSource());
 		}
-		
+
 		if (this._opts.items) {
 			var uid = 1;
 			var items = this._opts.items.map(function (item) {
@@ -51,9 +59,9 @@ var SelectBox = exports = Class(Widget, function(supr) {
 		}
 
 		this.initMouseEvents(this._el);
-		$.onEvent(this._el, 'change', this, '_onSelect');
+		$.onEvent(this._select, 'change', this, '_onSelect');
 	}
-	
+
 	this._onSelect = function() {
 		var item = this._dataSource.get(this.getValue());
 		if (item) {
@@ -73,12 +81,12 @@ var SelectBox = exports = Class(Widget, function(supr) {
 			this.onUpdateItem(key, item);
 		}, this);
 	}
-	
+
 	this.defaultRenderer = function(item) {
 		var key = this._dataSource.getKey();
 		return item.displayName || item.label || item.title || item.text || item[key];
 	}
-	
+
 	this.onUpdateItem = function(id, item) {
 		var el = this._items[id];
 		var keyField = this._dataSource.getKey();
@@ -91,7 +99,7 @@ var SelectBox = exports = Class(Widget, function(supr) {
 
 		if (!el) {
 			var el = this._items[id] = $.create({tag: 'option'});
-			$.insertBefore(this._el, el);
+			$.insertBefore(this._select, el);
 		}
 
 		el.setAttribute('value', item[keyField]);
@@ -99,7 +107,7 @@ var SelectBox = exports = Class(Widget, function(supr) {
 		el.innerText = (typeof renderer === 'string' ? item[renderer] : renderer(item))
 		el.value = item[keyField];
 	}
-	
+
 	this.onRemoveItem = function(id) {
 		var el = this._items[id];
 		var prevValue = this.getValue();
@@ -114,8 +122,8 @@ var SelectBox = exports = Class(Widget, function(supr) {
 	}
 
 
-	this.setName = function(name) { this._el.name = name; }
-	this.setValue = function(value) { this._el.value = value; }
+	this.setName = function(name) { this._select.name = name; }
+	this.setValue = function(value) { this._select.value = value; }
 
-	this.getValue = function() { return this._el.value; }
+	this.getValue = function() { return this._select.value; }
 });
