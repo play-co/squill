c6e1cc389a7058015c66a0fe630236229d2df39a
Martin Hunt <martin@gameclosure.com>
2014-07-14 02:24:56 -0700

    add formatter option

--- TextInput.js
+++ TextInput.js
@@ -1,11 +1,10 @@
-jsio('from util.browser import $');
-jsio('import .Widget, .global');
+from util.browser import $;
+import .Widget;
 
 var TextInput = exports = Class(Widget, function(supr) {
 	this._type = 'text';
 	this._css = 'textInput';
-	this._class = (global.getWidgetPrefix() === null) ? '' : (global.getWidgetPrefix() + this._css);
-	
+
 	this.init = function(opts) {
 		opts = merge(opts, {
 			name: '',
@@ -13,7 +12,7 @@ var TextInput = exports = Class(Widget, function(supr) {
 			type: 'text',
 			multiline: false
 		});
-		
+
 		this._def = {
 			children: [
 				{tag: opts.multiline ? 'textarea' : 'input', id: '_input', attrs: {
@@ -21,7 +20,6 @@ var TextInput = exports = Class(Widget, function(supr) {
 					value: opts.value,
 					name: opts.name
 				}, style: merge(opts.textStyle, {
-					display: 'block',
 					// width: '100%',
 					// height: '100%',
 					MozBoxSizing: 'border-box',
@@ -32,14 +30,14 @@ var TextInput = exports = Class(Widget, function(supr) {
 			],
 			style: {position: 'relative'}
 		};
-		
+
 		if (opts.prefixLabel) {
 			this._def.children.unshift({id: '_label', text: opts.label});
 		}
-		
+
 		supr(this, 'init', [opts]);
 	}
-	
+
 	this.buildWidget = function() {
 		var el = this._el;
 		var type = this._opts.type;
@@ -58,13 +56,13 @@ var TextInput = exports = Class(Widget, function(supr) {
 					zIndex: 1
 				}
 			});
-			
+
 			this._overlay.addEventListener('click', bind(this, function() {
 				$.hide(this._overlay);
 				this._input.focus();
 			}), true);
 		}
-		
+
 		if (!this._opts.prefixLabel || this._opts.placeholder) {
 			var label = this.getI18n('label');
 			if (this._input.getAttribute('placeholder') === null) {
@@ -73,34 +71,33 @@ var TextInput = exports = Class(Widget, function(supr) {
 				this._placeholder = $.create({
 					tag: 'button',
 					text: label,
-					className: global.getWidgetPrefix() + 'textInputLabel',
 					style: {position: 'absolute'},
 					parent: el
-				});	
+				});
 			}
 		}
-		
+
 		this.initMouseEvents(el);
 		this.initFocusEvents(this._input);
 		this.initKeyEvents(this._input);
 	}
 
 	this.getInputElement = function () { return this._input; }
-	
+
 	this.setName = function(name) {
 		supr(this, 'setName', arguments);
-		
+
 		if (this._input) { this._input.name = name; }
 	}
-	
+
 	this.setValue = function(value) { this._value = this._input.value = value; }
 	this.getValue = function() { return this._input.value; }
-	
+
 	this.onKeyDown = function() {
 		supr(this, 'onKeyDown', arguments);
 		if (this._placeholder) { $.hide(this._placeholder); }
 	}
-	
+
 	this.onKeyUp = function() {
 		supr(this, 'onKeyUp', arguments);
 		this.checkLabel();
@@ -113,7 +110,7 @@ var TextInput = exports = Class(Widget, function(supr) {
 		evt.stopPropagation();
 //		$.stopEvent(evt);
 	}
-	
+
 	this.onKeyPress = function(e) {
 		supr(this, 'onKeyPress', arguments);
 		if (e.keyCode == 13) {
@@ -121,35 +118,54 @@ var TextInput = exports = Class(Widget, function(supr) {
 		}
 		this.checkValue();
 	}
-	
+
 	this.onBlur = function() {
 		supr(this, 'onBlur');
 		this.checkLabel();
-		
+
 		if (this._overlay) {
 			$.show(this._overlay);
 		}
 	}
-	
+
+	this.isValid = function () { return this._isValid; }
+
 	this.checkValue = function() {
-		if (this._value != this._input.value) {
-			this._value = this._input.value;
+		var value = this._input.value;
+		var formatter = this._opts.formatter;
+		if (formatter) {
+			value = formatter(value);
+		}
+
+		if (value == INVALID_VALUE) {
+			this._isValid = false;
+			$.addClass(this._el, 'invalid');
+			return;
+		}
+
+		this._isValid = true;
+		$.removeClass(this._el, 'invalid');
+		if (this._value != value) {
+			this._value = value;
+			this._input.value = value;
 			this.publish('change', this._value);
 			this.publish('ValueChange', this._value);
 		}
 	}
-	
+
 	this.checkLabel = function() {
 		if(this._placeholder && /^\s*$/.test(this._input.value)) {
 			$.show(this._placeholder);
 		}
 	}
-	
+
 	this.onClick = function() {
 		supr(this, 'onClick');
-		
+
 		//setTimeout(bind(this._input, 'focus'), 100);
 	}
 });
+
+var INVALID_VALUE = exports.INVALID_VALUE = {};
 
 Widget.register(TextInput, 'TextInput');
