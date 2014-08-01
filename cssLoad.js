401666499552553bd30a56062a1f2d2c557b50f7
Martin Hunt <martin@gameclosure.com>
2014-08-01 12:58:10 -0700

    use onload/onerror handlers for better css support

--- cssLoad.js
+++ cssLoad.js
@@ -8,56 +8,50 @@ exports.get = function(opts, cb) {
 	if (typeof opts == 'string') {
 		opts = {url: opts};
 	}
-	
-	var parent = opts.el || document.getElementsByTagName('head')[0];
-	
-	var uri = std.uri.relativeTo(opts.url, window.location);
-	var host = uri.getHost ? uri.getHost() : false;
-	if (host && (host != window.location.hostname)) {
-		var el = $({tag: 'link'});
-		el.type = 'text/css';
-		el.rel = 'stylesheet';
-		el.href = opts.url;
-		parent.appendChild(el);
-		setTimeout(cb, 500);
-	} else {
-		util.ajax.get({
-			url: opts.url
-		}, function(err, content) {
-			if (err) {
-				logger.error('could not fetch css at', opts.url);
-				cb && cb(err);
-				return;
-			}
 
-			var el = $({tag: 'style', text: content, parent: parent});
+	var win = (opts.win || window);
+	var doc = win.document;
+	var loc = win.location;
 
-			if (window.DEV_MODE) {
-				exports._styleTags.push({
-					el: el,
-					src: opts.url
-				});
-			}
-			
+	var parent = opts.el || doc.getElementsByTagName('head')[0];
+
+	// use a script tag
+	var el = $({tag: 'link'});
+	el.type = 'text/css';
+	el.rel = 'stylesheet';
+	if ('onload' in el) {
+		el.onload = function () {
 			setTimeout(function () {
-				cb && cb(err, el);
-			}, 0);
-		});
+				cb && cb(null);
+			}, 0); // wait for reflow
+		};
+
+		el.onerror = function (e) {
+			cb && cb(e);
+		};
+	} else {
+		// don't know when it loads :(
+		setTimeout(function () {
+			cb && cb();
+		}, 500);
 	}
+
+	var url = std.uri.relativeTo(opts.url, window.location.toString());
+
+	exports._styleTags.push({
+		el: el,
+		src: url
+	});
+
+	parent.appendChild(el);
+	el.href = url;
 }
 
-if (window.DEV_MODE) {
-	exports._styleTags = [];
-	window.reloadCSS = function() {
-		for (var i = 0, s; s = exports._styleTags[i]; ++i) {
-			util.ajax.get({url: s.src}, bind(this, function(s, err, content) {
-				if (!err) { $.setText(s.el, content); }
-			}, s));
-		}
+exports._styleTags = [];
+exports.reloadCSS = function() {
+	for (var i = 0, s; s = exports._styleTags[i]; ++i) {
+		util.ajax.get({url: s.src}, bind(this, function(s, err, content) {
+			if (!err) { $.setText(s.el, content); }
+		}, s));
 	}
-	window.addEventListener('message', function(evt) {
-		if (evt.data == 'squill.cssLoad.reload()') {
-			reloadCSS();
-		}
-	}, true);
 }
